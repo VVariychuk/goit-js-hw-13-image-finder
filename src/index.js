608833bb88a58dc6c393cb0a4dceb2d2c6880124
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-04-16 00:22:28 +0300

    creating gallery

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './styles.css';
+import imageList from './templates/image-list.hbs'
+import FetchApi from './js/fetchApi'
+import LoadMore from './js/loadMoreBtn'
+
+const refs = {
+    form: document.getElementById('search-form'),
+    gallery: document.querySelector('.gallery'),
+    searchBtn: document.querySelector('.searchBtn'),
+}
+
+const loadMore = new LoadMore({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+})
+
+const fetchApi = new FetchApi();
+
+refs.form.addEventListener('submit', onSearch)
+loadMore.refs.button.addEventListener('click', fetchItems);
+
+function onSearch(e) {
+    e.preventDefault()
+
+    fetchApi.query = e.currentTarget.elements.query.value
+    loadMore.show()
+    fetchApi.resetPage()
+    resetGallery()
+    fetchItems()
+}
+
+function fetchItems() {
+    loadMore.disable()
+    fetchApi.fetchImg()
+        .then(hits => {
+            if (hits.length < 12) {
+                loadMore.hide()
+                createMarkup(hits)
+            } else {
+                createMarkup(hits)
+                loadMore.enable()
+            }
+        })
+    }
+
+function createMarkup(hits) {
+    refs.gallery.insertAdjacentHTML('beforeend', imageList(hits))
+ }
+
+
+function resetGallery() {
+    refs.gallery.innerHTML = ''
+}
+ 
---
+++ src/js/fetchApi.js
@@ -0,0 +1,36 @@
+const API_KEY = '19836909-16e58b6603fc5616f8f6edf37';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class ImgFinderServise {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImg() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
